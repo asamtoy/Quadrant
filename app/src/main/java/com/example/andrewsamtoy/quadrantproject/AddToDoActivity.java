9ffa1c1ed990d847e82805e1cc4b8bf6860df0f2
Andrew Samtoy <asamtoy@gmail.com>
2017-09-26 21:29:47 +0100

    Tuesday evening commit

--- app/src/main/java/com/example/andrewsamtoy/quadrantproject/AddToDoActivity.java
+++ app/src/main/java/com/example/andrewsamtoy/quadrantproject/AddToDoActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -18,7 +19,7 @@ public class AddToDoActivity extends AppCompatActivity {
 
     ToDo toDo;
 
-    EditText toDoTitle;
+    EditText toDoTitleInput;
     EditText toDoDetails;
     ToggleButton toggleImportant;
     ToggleButton toggleUrgent;
@@ -30,21 +31,30 @@ public class AddToDoActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_to_do);
 
-        Button saveToList = (Button) findViewById(R.id.saveToList);
-        saveToList.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(AddToDoActivity.this, MainActivity.class));
-            }
+        toDoTitleInput = (EditText) findViewById(R.id.toDoTitleInput);
+        toDoDetails = (EditText) findViewById(R.id.toDoDetails);
+        toggleImportant = (ToggleButton) findViewById(R.id.toggleImportant);
+        toggleUrgent = (ToggleButton) findViewById(R.id.toggleUrgent);
+        saveToList = (Button) findViewById(R.id.saveToList);
 
-        });
     }
-    public void onSaveButtonClicked(View button) {
-        Intent intent = new Intent(this, MainActivity.class);
-        String title = toDoTitle.getText().toString();
-        intent.putExtra("This is the title", title);
 
-        startActivity(intent);
+    public void onSaveButtonClicked(View button) {
+        Log.d("onSaveButton ", "Clicked");
+        String title = toDoTitleInput.getText().toString();
+        String details = toDoDetails.getText().toString();
+        Boolean isImportant = toggleImportant.isChecked();
+        Boolean isUrgent = toggleUrgent.isChecked();
+
+        Log.d("To Do Title", title.toString());
+        Log.d("To Do Details", details.toString());
+        Log.d("Is this important? ", isImportant.toString());
+        Log.d("Is this urgent? ", isUrgent.toString());
+
+//        startActivity(intent);
+//        Intent intent = new Intent(this, MainActivity.class);
+//        String title = toDoTitleInput.getText().toString();
+//        intent.putExtra("This is the title", title);
 
     }
 
@@ -73,5 +83,15 @@ public class AddToDoActivity extends AppCompatActivity {
 //        startActivity(intent);
 //
 //    }
+    //        saveToList.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                String title = toDoTitle.getText().toString();
+////        intent.putExtra("This is the title", title);
+//                Log.d("This is the title: ", title);
+//
+////                startActivity(new Intent(AddToDoActivity.this, MainActivity.class));
+//            }
+//        });
 
 }

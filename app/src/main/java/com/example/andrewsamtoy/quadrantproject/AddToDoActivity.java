090a4349ea12a6795d73b53044db3d1474786a92
Andrew Samtoy <asamtoy@gmail.com>
2017-09-26 16:20:42 +0100

    Tuesday 16:20

--- app/src/main/java/com/example/andrewsamtoy/quadrantproject/AddToDoActivity.java
+++ app/src/main/java/com/example/andrewsamtoy/quadrantproject/AddToDoActivity.java
@@ -1,6 +1,7 @@
 package com.example.andrewsamtoy.quadrantproject;
 
 import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -10,6 +11,8 @@ import android.widget.TextView;
 import android.widget.ToggleButton;
 
 import org.w3c.dom.Text;
+
+import static android.R.attr.button;
 
 public class AddToDoActivity extends AppCompatActivity {
 
@@ -24,15 +27,27 @@ public class AddToDoActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
-//        toDo = new ToDo();
-
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_to_do);
 
+        Button saveToList = (Button) findViewById(R.id.saveToList);
+        saveToList.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(AddToDoActivity.this, MainActivity.class));
+            }
 
+        });
+    }
+    public void onSaveButtonClicked(View button) {
+        Intent intent = new Intent(this, MainActivity.class);
+        String title = toDoTitle.getText().toString();
+        intent.putExtra("This is the title", title);
+
+        startActivity(intent);
 
     }
+
 
 //    public void setToDoTitle(Text text) {
 //        String toDoTitleInput = toDoTitle.getText().toString();
